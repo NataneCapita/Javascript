cb11cd494536cd3d28b3c57b048541ac38903d72
Natane Capita <natanecapita20@gmail.com>
2024-08-13 00:21:35 +0000

    Initial commit

---
+++ Aula 11/Exe009.js
@@ -0,0 +1,7 @@
+var país='França'
+console.log(`vivendo ${país}`)
+if(país !='Brasil') {
+    console.log('voce e estrangeiro')
+} else {
+    console.log('voce e brasileiro ')    
+}
---
+++ Aula 12/Exercicio.js
@@ -0,0 +1,9 @@
+var idade= 15
+if(idade <18){
+    console.log ('menor de idade nao vota')
+} else{
+    console.log ('maior idade vota')
+
+} else{
+    console.log('')
+}
