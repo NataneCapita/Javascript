23b640594725cd3952a57785039e4b495c3b7f03
Natane Capita <natanecapita20@gmail.com>
2024-11-20 10:53:22 +0000

    atualizando os exercicioas

--- Condições 11/aula11/Exe008.js
+++ Condições 11/aula11/Exe008.js
@@ -1,6 +1,13 @@
-var vel =78
-console.log(`A velocidade do seu carro e ${vel} km/h`)
-if(vel>60){ 
-    console.log(`voce ultrapassou a velocidade permitida .Multado`)
-}
-console.log(`dirija sempre usando cinto de segurança`)
+
+
+
+
+    let pais = 'Angola'
+    if (pais =='Angolano'){
+        console.log('Angolano')
+
+    }else{
+        console.log('Estrangeiro')
+    }
+
+
--- Condições 11/aula11/Exe009.js
+++ Condições 11/aula11/Exe009.js
@@ -1,7 +1,12 @@
-var país='França'
+const { vel } = require("./Exe008")
+
+ var país='França'
 console.log(`vivendo ${país}`)
 if(país !='Brasil') {
     console.log('voce e estrangeiro')
 } else {
     console.log('voce e brasileiro ')    
+}
+if (vel > '40') {
+    console.log(`voce ultrapassou a velocidade permitida ,multado!`)
 }
