c98708f308dc91f525c3f262e2ebcc29e4aec9ec
Natane Capita <natanecapita20@gmail.com>
2024-12-21 11:24:33 +0000

    exercitando

--- Condições 11/aula11/Exe008.js
+++ Condições 11/aula11/Exe008.js
@@ -1,8 +1,10 @@
-var idade=18
+var idade=24
 
-if (idade <17){
-    console.log('nao vota')
+if (idade <18){
+    console.log('nao vota');
+}else if (idade<=21){
+    console.log('voto opcional');
 }else{
-    console.log('voto exigido')
+    console.log('voto obrigatorio');
 }
 
