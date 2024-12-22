2efb7bc574cb22b5df7d3ecaa011bb8be2638cf4
Natane Capita <natanecapita20@gmail.com>
2024-12-22 15:54:07 +0000

    exercitando

--- Aula 13/exerci14/script.js
+++ Aula 13/exerci14/script.js
@@ -1,1 +1,15 @@
-alert("ola") 
+function carregar () {
+    var msg =window.document.getElementById('msg')
+    var img=window.document.getElementById('imagem')
+    var data=new date()
+    var hora=data.gethours()
+    msg.innerHTML=(`agora sao ${hora} horas`)
+if (hora>=0 && hora <12){
+    img.src='manha.png'
+}else if(hora>=12 && hora<18){
+    img.src='tarde.png'
+}else{
+    img.src='noite.png'
+}
+}   
+
