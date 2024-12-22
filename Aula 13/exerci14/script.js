bf5571db4f80be8f19dfcd40fd676364880e5f32
Natane Capita <natanecapita20@gmail.com>
2024-12-22 17:47:21 +0000

    criando programa hora do dia

--- Aula 13/exerci14/script.js
+++ Aula 13/exerci14/script.js
@@ -1,15 +1,23 @@
-function carregar () {
+
+function carregar() {
     var msg =window.document.getElementById('msg')
-    var img=window.document.getElementById('imagem')
-    var data=new date()
-    var hora=data.gethours()
-    msg.innerHTML=(`agora sao ${hora} horas`)
-if (hora>=0 && hora <12){
-    img.src='manha.png'
-}else if(hora>=12 && hora<18){
-    img.src='tarde.png'
+    var img =window.document.getElementById('imagem')
+    var data =new Date()
+    //var hora=8
+    var hora =data.getHours()
+    var min=data.getMinutes()
+    msg.innerHTML=(`Agora sao ${hora} horas e ${min} minutos.`)
+if (hora >= 0 && hora <12){
+    img.src='manha.png';
+    document.body.style.background='#a1a99f '
+    
+}else if(hora >=12 && hora<18){
+    img.src='tarde...png'
+    document.body.style.background='#a86530'
 }else{
     img.src='noite.png'
+    document.body.style.background= '#5d2518'
 }
+
 }   
 
